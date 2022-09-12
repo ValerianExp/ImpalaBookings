57b8e7609b230b5cb8511e865452755ead0b20e1
RstefaniB <rickies72@gmail.com>
2022-09-12 18:05:20 +0200

    CRUD customer

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.use("/", index);
 const hotels = require("./routes/hotels.routes");
 app.use('/hotels', hotels);
 
+const users = require("./routes/users.routes");
+app.use('/users', users);
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
