49bac9fab0c5d24bd8bb7b0c3c2f4a8f492a9721
RedValerian <r.pino.camacho@gmail.com>
2022-09-13 11:13:14 +0200

    Maps beginning

--- models/hotel.model.js
+++ models/hotel.model.js
@@ -3,10 +3,11 @@ const { Schema, model } = require('mongoose');
 const hotelSchema = new Schema({
     hotelId: String,
     roomTypes: [{ type: Schema.Types.ObjectId, ref: 'Rooms' }],
+    location: { type: { type: String }, coordinates: [Number] },
 }, {
     timestamps: true,
     versionKey: false
 });
-
+hotelSchema.index({ location: '2dsphere' });
 const hotelModel = model('Hotels', hotelSchema);
 module.exports = hotelModel;
---
+++ public/js/map.js
@@ -0,0 +1,51 @@
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+        console.log("map JS imported successfully!");
+
+        function createMarker(center, map, title) {
+            new google.maps.Marker({
+                position: center,
+                map: map,
+                title: title
+            });
+        }
+
+        function geocode(map) {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    function (position) {
+                        const pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+                        map.setCenter(pos);
+                        createMarker(pos, map, "You are here");
+                    },
+                    function () {
+                        console.log("Error in the geolocation service.");
+                        // handleLocationError(true, map.getCenter());
+                    }
+                );
+            }
+            else {
+                console.log("Browser does not support geolocation.");
+                // handleLocationError(false, map.getCenter());
+            }
+        }
+
+        function placeHotel(hotel, map) {
+            const hotelLocation = {
+                lat: hotel.location.coordinates[1],
+                lng: hotel.location.coordinates[0]
+            };
+            createMarker(hotelLocation, map, hotel.name);
+        }
+
+        function placeHotels(hotels, map) {
+            hotels.forEach((hotel) => {
+                placeHotel(hotel, hotel.location);
+            });
+        }
+    }
+);
